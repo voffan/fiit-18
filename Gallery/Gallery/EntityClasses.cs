d143d2f3cb22f799d23777e0d9f2fb74aba2d388
Kim1350 <70932884+Kim1350@users.noreply.github.com>
2021-02-09 12:14:19 +0900

    classes c#

--- Gallery/Gallery/EntityClasses.cs
+++ Gallery/Gallery/EntityClasses.cs
@@ -11,7 +11,20 @@ using System.Data.Entity.Spatial;
 namespace Gallery
 {
     public enum StatusSell { sold, cancel, progress }
-    public class Employee
+    public enum Status {active, inactive }
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Departament
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+    public class Person
     {
         [Key]
         public int Id { get; set; }
@@ -26,22 +39,86 @@ namespace Gallery
         [StringLength(100)]
         public string Phone { get; set; }
     }
+    public class Employee:Person
+    {
+        public int IdDep { get; set; }
+        [ForeignKey("IdDep")]
+        public Departament Departament { get; set; }
+        public Status Status { get; set; }
+    }
 
     public class Sell
     {
+        [Key]
+        public int Id { get; set; }
         public long Price { get; set; }
         public DateTime Date { get; set; }
         public StatusSell Status { get; set; }
+    }
+    public class Exhibition
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NameExhibition { get; set; }
+        public Country Country { get; set; }
+        public string City { get; set; }
+        public DateTime Date { get; set; }
     }
 
     public class Journal
     {
         public Employee Employee { get; set; }
         public DateTime Date { get; set; }
-        //доделать
+        public Departament Source { get; set; }
+        public Departament Destination { get; set; }
+        public Exhibition Exhibition { get; set; }
+
+    }
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NameGenre { get; set; }
+    }
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(255)]
+        public string Surname { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string Middle_Name { get; set; }
+        public int BirthYear { get; set; }
+        public int DeathYera { get; set; }
     }
     public class PaintingCost
     {
         public int Cost { get; set; }
+    }
+    public class Painting
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdGenre { get; set; }
+        [ForeignKey("IdGenre")]
+        public Genre Genre { get; set; }
+        public int IdExhibition { get; set; }
+        [ForeignKey("IdExhibition")]
+        public Exhibition Exhibition { get; set; }
+        public string NamePainting { get; set; }
+        public int IdArtist { get; set; }
+        [ForeignKey("IdArtist")]
+        public Artist Artist { get; set; }
+
+    }
+    public class Customer:Person
+    {
+        [Key]
+        public int Id { get; set;  }
+        public int IdSell { get; set; }
+        [ForeignKey("IdSell")]
+        public Sell Sell { get; set; }
     }
 }
