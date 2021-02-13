f02ee338bc00d40bcf55faafc9d3cb503ee65bac
zereckt <43085576+zereckt@users.noreply.github.com>
2021-02-13 12:08:25 +0900

    Add files via upload

---
+++ Sport/Sport/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+
+namespace Sport
+{
+    
+    public class Sportsman
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Judge
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Organizer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+    public class Administrator
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+    public class Competition
+    {
+    [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+
+}
