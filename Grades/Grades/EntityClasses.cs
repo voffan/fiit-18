783462a0887b9e199b29d4b3ee71e67452f84f81
voffan <vovan_evv@rambler.ru>
2021-02-25 12:03:32 +0900

    Classes fixes

--- Grades/Grades/EntityClasses.cs
+++ Grades/Grades/EntityClasses.cs
@@ -20,21 +20,16 @@ namespace Grades
         public string Name { get; set; }
         [StringLength(50)]
         public string MiddleName { get; set; }
-        [StringLength(50)]
         public DateTime DateOfBirth { get; set; }
-        public string Address { get; set; }
         [StringLength(255)]
+        public string Address { get; set; }
         public int Phone { get; set; }
-        [StringLength(15)]
 
         public int ClassId { get; set; }
         [ForeignKey("ClassId")]
         public Class Class { get; set; }
 
-        public int GroupId { get; set; }
-        [ForeignKey("GroupId")]
-        public Group Group { get; set; }
-
+        public virtual ICollection<Group> Groups { get; set; }
         public virtual ICollection<Table> Tables { get; set; }
     }
 
@@ -43,9 +38,8 @@ namespace Grades
         [Key]
         public int Id { get; set; }
         public char Symbol { get; set; }
-        [StringLength(2)]
         public int Year { get; set; }
-        public string SchoolId { get; set; }
+        public int SchoolId { get; set; }
         [ForeignKey("SchoolId")]
         public School School { get; set; }
         public int EmployeeId { get; set; }
@@ -60,15 +54,13 @@ namespace Grades
     {
         [Key]
         public int Id { get; set; }
-        [StringLength(150)]
+        [StringLength(255)]
         public string Name { get; set; }
         [StringLength(255)]
         public string Address { get; set; }
         [StringLength(255)]
         public string Email { get; set; }
-        [StringLength(255)]
         public int Phone { get; set; }
-        [StringLength(15)]
 
         public virtual ICollection<Employee> Employeers { get; set; }
         public virtual ICollection<Class> Classes { get; set; }
@@ -84,19 +76,21 @@ namespace Grades
         public string Name { get; set; }
         [StringLength(50)]
         public string MiddleName { get; set; }
-        [StringLength(50)]
         public DateTime DateOfBirth { get; set; }
-        public string Address { get; set; }
         [StringLength(255)]
+        public string Address { get; set; }
         public int Phone { get; set; }
-        [StringLength(15)]
+        
         public int PositionId { get; set; }
         [ForeignKey("PositionId")]
         public Position Position { get; set; }
 
-        public ICollection<Course> Course { get; set; }
-        public ICollection<Class> Classes { get; set; }
-        public virtual ICollection<School> Schools { get; set; }
+        public int SchoolId { get; set; }
+        [ForeignKey("SchoolId")]
+        public School School { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual ICollection<Class> Classes { get; set; }
     }
 
     public class Course
@@ -113,6 +107,9 @@ namespace Grades
         public int? GroupId { get; set; }
         [ForeignKey("GroupId")]
         public Group Subgroup { get; set; }
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
 
         public ICollection<Table> Tables { get; set; }
     }
@@ -123,7 +120,6 @@ namespace Grades
         public int Id { get; set; }
         [StringLength(150)]
         public string Name { get; set; }
-        [StringLength(50)]
         public int SubjectId { get; set; }
         [ForeignKey("SubjectId")]
         public Subject Subject { get; set; }
@@ -134,20 +130,25 @@ namespace Grades
         public virtual ICollection<Student> Students { get; set; }
     }
 
-
-    }
-
     public class Table
     {
+        [Key]
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        [ForeignKey("StudentId")]
+        public Student Student { get; set; }
+        public int AcademicYearId { get; set; }
+        [ForeignKey("AcademicYearId")]
+        public AcademicYear AcademicYear { get; set; }
+        public int CheckPointId { get; set; }
+        [ForeignKey("CheckPointId")]
+        public CheckPoint CheckPoint { get; set; }
+        public int CourseId { get; set; }
+        [ForeignKey("CourseId")]
+        public Course Course { get; set; }
         public int MarkId { get; set; }
         [ForeignKey("MarkId")]
         public Mark Mark { get; set; }
-
-
-        public virtual ICollection<Student> Students { get; set; }
-        public virtual ICollection<Course> Courses { get; set; }
-        public virtual ICollection<CheckPoint> CheckPoints { get; set; }
-        public virtual ICollection<AcademicYear> AcademicYears { get; set; }
     }
 
 
@@ -165,30 +166,34 @@ namespace Grades
     {
         [Key]
         public int Id { get; set; }
-        [StringLength(150)]
-        public string Name { get; set; }
         [StringLength(50)]
+        public string Name { get; set; }
     }
 
     public class Subject
     {
         [Key]
         public int Id { get; set; }
-        [StringLength(150)]
-        public string Name { get; set; }
         [StringLength(50)]
+        public string Name { get; set; }
     }
 
     public class CheckPoint
     {
-        public string Name { get; set; }
+        [Key]
+        public int Id { get; set; }
         [StringLength(50)]
+        public string Name { get; set; }
+        
         public virtual ICollection<Table> Tables { get; set; }
     }
-public class AcademicYear
-{
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
+    public class AcademicYear
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
 
-    public virtual ICollection<Table> Tables { get; set; }
+        public virtual ICollection<Table> Tables { get; set; }
+    }
 }
