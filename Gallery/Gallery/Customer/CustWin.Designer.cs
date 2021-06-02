0fb43b933a436804fc4637135716e3d4a3a5c5a3
Kim1350 <70932884+Kim1350@users.noreply.github.com>
2021-06-02 21:08:21 +0900

    231

--- Gallery/Gallery/Customer/CustWin.Designer.cs
+++ Gallery/Gallery/Customer/CustWin.Designer.cs
@@ -56,7 +56,7 @@ namespace Gallery
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(12, 280);
+            this.button1.Location = new System.Drawing.Point(12, 279);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 60);
             this.button1.TabIndex = 0;
