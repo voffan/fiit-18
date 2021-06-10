4a0367dd187e675dcbdfe13528c4752a53a03c6b
Dmitriev Albert <dmitriev.alik1999@gmail.com>
2021-06-10 13:35:55 +0900

    fix

--- Grades/Grades/TeacherMenu.Designer.cs
+++ Grades/Grades/TeacherMenu.Designer.cs
@@ -53,6 +53,7 @@ namespace Grades
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 24);
             this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // label1
             // 
--- Grades/Grades/TeacherMenu.cs
+++ Grades/Grades/TeacherMenu.cs
@@ -20,32 +20,41 @@ namespace Grades
 
         private void TeacherMenu_Load(object sender, EventArgs e)
         {
-            var classes = db.TeacherSubjects
-                       .Where(c => c.UserId == Authorization.User.Id)
-                       .OrderBy(c => c.ClassId);
-            comboBox1.DataSource = classes;
-
-            dataGridView1.DataSource = db.TeacherMarksTables.ToList();
-            dataGridView1.DataSource = db.Employees.ToList();
-            //dataGridView1.Columns[0].Visible = false;
-            //dataGridView1.Columns[1].HeaderText = "Фамилия";
-            //dataGridView1.Columns[2].HeaderText = "Имя";
-            //dataGridView1.Columns[3].HeaderText = "Отчество";
-            //dataGridView1.Columns[4].HeaderText = "Дата рождения";
-            //dataGridView1.Columns[5].HeaderText = "Адрес";
-            //dataGridView1.Columns[6].HeaderText = "Телефон";
-            //dataGridView1.Columns[7].Visible = false;
-            //dataGridView1.Columns[8].HeaderText = "Должность";
-            //dataGridView1.Columns[9].Visible = false;
-            //dataGridView1.Columns[10].HeaderText = "Школа";
-            //dataGridView1.Columns[11].Visible = false;
-            //dataGridView1.Columns[12].Visible = false;
+            var courses = db.Courses
+                    .Where(c => c.Employee.UserId == Authorization.User.Id)
+                    .OrderBy(c => c.Employee.UserId)
+                    .Select(c => c.Subject.Name)
+                    .ToList();
+            comboBox1.DataSource = courses;
+            //var classes = db.TeacherSubjects
+            //           .Where(c => c.UserId == Authorization.User.Id)
+            //           .OrderBy(c => c.ClassId)
+            //           .Select(c => c.ClassId);
+            //foreach (int ids in classes)
+            //    Console.WriteLine(ids);
+            //classes = classes.Distinct();
+            //comboBox1.DataSource = classes.ToList();
+            //comboBox1.DataSource = db.Classes
+            //           .Where(c => c.Id == classes.Class);
+            //dataGridView1.DataSource = db.TeacherMarksTables.ToList();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            var classes = db.Courses
+                    .Where(c => c.Employee.UserId == Authorization.User.Id)
+                    .Where(c => c.Subject.Id == Convert.ToInt32(comboBox1.SelectedValue))
+                    .OrderBy(c => c.Employee.UserId)
+                    .Select(c => new { id = c.Id, name = c.Class.Year })
+                    .ToList();
+            comboBox2.DataSource = classes;
+            comboBox2.DisplayMember = "name";
+            comboBox2.ValueMember = "id";
+        }
 
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var table = 
         }
     }
 }
